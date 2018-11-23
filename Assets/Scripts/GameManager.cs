a103b720aee8e700da7ea0a1d516db0dd48c3252
theshteves <stevenkneiser@gmail.com>
2018-11-23 01:17:49 -0500

    Added some assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [Range(0f, 10f)] public float gameSpeed = 1f;
+
+    public long numPlayers = 2;
+    public GameObject playerObject;
+    public Transform spawn;
+    public float spawnRadius = 10f;
+
+    void Awake() {
+        Time.timeScale = gameSpeed;
+        Instantiate(playerObject, spawn);
+    }
+	
+	// Update is called once per frame
+	void Update() {}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+    [Range(0f, 1000f)] public float speed = 1f;
+    [Range(.5f, 1f)] public float frameDrag = 1f;
+    public GameObject bulletObject;
+    [Range(0f, 1000f)] public float bulletSpeed = 1f;
+
+    private Rigidbody _body;
+
+	// Use this for initialization
+	void Awake() {
+        _body = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        Vector3 delta = Vector3.zero;
+        delta.x = Input.GetAxis("Horizontal");
+        delta.z = Input.GetAxis("Vertical");
+
+        _body.AddForce(delta * speed);
+        _body.velocity *= frameDrag;
+
+        // Look where you're headed
+        if (_body.velocity.magnitude > 0.1f) { this.transform.rotation = Quaternion.LookRotation(_body.velocity); }
+	}
+
+    void FixedUpdate() { if (Input.GetButtonDown("Jump")) { ShootBullet(); } }
+
+    void ShootBullet() {
+
+        // Spawn bullet in front of player
+        var bullet = Instantiate(bulletObject, this.transform);
+        bullet.transform.SetParent(null);
+        bullet.transform.localPosition += this.transform.forward;
+        bullet.GetComponent<Rigidbody>().velocity = this.transform.forward * bulletSpeed;
+    }
+}
